0f32a464f80690692c69f9484616548689486c2d
Néstor Salceda <nestor.salceda@gmail.com>
2016-10-19 00:42:24 +0200

    Use python like convention for private methods

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ class IllegalRateException { }
 
 class DistributionService {
   constructor(httpClient) {
-    this.httpClient = httpClient;
+    this._httpClient = httpClient;
   }
 
   setRates(iterationId, rates) {
     let result = rates.reduce((total, rate) => { return total + rate.rate}, 0);
     if (result != 100) throw new IllegalRateException();
-    this.httpClient.put('/iteration/' + iterationId + '/rates', rates);
+    this._httpClient.put('/iteration/' + iterationId + '/rates', rates);
     return {
       id: iterationId,
       rates: rates

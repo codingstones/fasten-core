239be9bc7039d59aff49d11404106c8d4923f3eb
Néstor Salceda <nestor.salceda@gmail.com>
2016-10-21 17:42:08 +0200

    Use an empty implementation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@
 
 class HttpClient {
   put(url, payload) {
-    //fake implementation!!!
-    var pify = require('pify');
-    var request = pify(require('request'));
-    return request.get('http://mosica.es/api/1/gigs/4155');
   }
 }
 
@@ -21,11 +17,10 @@ class DistributionService {
         reject('Rates does not sum 100%');
       }
 
-      this._httpClient.put('/iteration/' + iterationId + '/rates', rates).then((response) => {
-        resolve({
-          id: iterationId,
-          rates: rates
-        });
+      this._httpClient.put('/iteration/' + iterationId + '/rates', rates);//.then((response) => {
+      resolve({
+        id: iterationId,
+        rates: rates
       });
     });
   }
